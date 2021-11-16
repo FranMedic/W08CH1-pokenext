7cb04d1de5a817fe03190455e14363567bbe7c4a
Franny Medico <m.francisca.medico@gmail.com>
2021-11-16 20:33:05 +0100

    added pokedexSSG page

--- pages/pokedexSSG/index.js
+++ pages/pokedexSSG/index.js
@@ -1,9 +1,60 @@
-const PokedexSsg = () => {
+import Link from "next/link";
+import styles from "../../styles/Pokemon.module.css";
+const PokedexSsg = ({ pokemons }) => {
   return (
     <>
-      <h1>HOLA BB SSG</h1>
+      {" "}
+      <ul className={styles.menu}>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/pokemon">
+            <a>Pokemon</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/pokedexSSR">
+            <a>My Pokedex SSR</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/pokedexSSG">
+            <a>My Pokedex SSG</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/pokedexISR">
+            <a>My Pokedex ISR</a>
+          </Link>
+        </li>
+      </ul>
+      <h1 className={styles.title}>Welcome to Pokemons SSG </h1>
+      <ul className={styles.listContainer}>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
 
+export const getStaticProps = async () => {
+  const response = await fetch("https://pokeapi-menchu.herokuapp.com/pokemon");
+  const pokemons = await response.json();
+
+  if (!pokemons) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { pokemons },
+  };
+};
 export default PokedexSsg;
